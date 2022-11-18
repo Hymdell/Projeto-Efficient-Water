260d3f23f5c5232246b4b20bd849a11493622552
Cristopher Machado <cristopherpiussi@gmail.com>
2022-11-18 14:16:53 -0300

    mudança de tudo para o main telas dinamicas adição e edição do menu lateral

--- lib/main.dart
+++ lib/main.dart
@@ -1,147 +1,371 @@
 import 'package:flutter/material.dart';
-import 'package:projetoirrigacao/alface.dart';
-import 'package:projetoirrigacao/batata.dart';
-import 'package:projetoirrigacao/tomate.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const String _title = 'Projeto Irrigação';
+  static const appTitle = 'Projeto Irrigação';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar:
-            AppBar(backgroundColor: Colors.green, title: const Text(_title)),
-        body: const Center(
-          child: MyStatefulWidget(),
+    return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.green, title: Text(title)),
+      body: const Center(
+        child: Text('My Page!'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const SizedBox(
+              height: 100,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    image: DecorationImage(
+                        image: AssetImage('assets/plantacao.jpg'),
+                        fit: BoxFit.cover)),
+                child: Text(
+                  'Culturas',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: const Text('Alface'),
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.black, width: 0.5),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                leading: ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    minWidth: 44,
+                    minHeight: 44,
+                    maxWidth: 64,
+                    maxHeight: 64,
+                  ),
+                  child: Image.asset('assets/alface.png', fit: BoxFit.cover),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Cultura(
+                            'Alface', '0', '0', 0, 0, Colors.green)),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: const Text('Batata'),
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.black, width: 0.5),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                leading: ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    minWidth: 44,
+                    minHeight: 44,
+                    maxWidth: 64,
+                    maxHeight: 64,
+                  ),
+                  child: Image.asset('assets/batata.png', fit: BoxFit.cover),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Cultura(
+                            'Batata', '0', '0', 0, 0, Colors.yellow)),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: const Text('Tomate'),
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.black, width: 0.5),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                leading: ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    minWidth: 44,
+                    minHeight: 44,
+                    maxWidth: 64,
+                    maxHeight: 64,
+                  ),
+                  child: Image.asset('assets/tomate.png', fit: BoxFit.cover),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Cultura(
+                            'Tomate', '0', '0', 0, 0, Colors.red)),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: const Text('Rúcula'),
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.black, width: 0.5),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                leading: ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    minWidth: 44,
+                    minHeight: 44,
+                    maxWidth: 64,
+                    maxHeight: 64,
+                  ),
+                  child: Image.asset('assets/rucula.png', fit: BoxFit.cover),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Cultura(
+                            'Rúcula', '0', '0', 0, 0, Colors.green)),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: const Text('Radite'),
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.black, width: 0.5),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                leading: ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    minWidth: 44,
+                    minHeight: 44,
+                    maxWidth: 64,
+                    maxHeight: 64,
+                  ),
+                  child: Image.asset('assets/radite.png', fit: BoxFit.cover),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Cultura(
+                            'Radite', '0', '0', 0, 0, Colors.green)),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: const Text('Beterraba'),
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.black, width: 0.5),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                leading: ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    minWidth: 44,
+                    minHeight: 44,
+                    maxWidth: 64,
+                    maxHeight: 64,
+                  ),
+                  child: Image.asset('assets/beterraba.png', fit: BoxFit.cover),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Cultura(
+                            'Beterraba', '0', '0', 0, 0, Colors.purple)),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: const Text('Cenoura'),
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.black, width: 0.5),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                leading: ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    minWidth: 44,
+                    minHeight: 44,
+                    maxWidth: 64,
+                    maxHeight: 64,
+                  ),
+                  child: Image.asset('assets/cenoura.png', fit: BoxFit.cover),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Cultura(
+                            'Cenoura', '0', '0', 0, 0, Colors.orange)),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({super.key});
+class Cultura extends StatefulWidget {
+  final String titulo, dias, temperaturaIdeal;
+  final int umidadeAtual, umidadeNecessaria;
+  final Color cor;
+  const Cultura(this.titulo, this.dias, this.temperaturaIdeal,
+      this.umidadeAtual, this.umidadeNecessaria, this.cor,
+      {super.key});
 
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  State<Cultura> createState() => _CulturaState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+class _CulturaState extends State<Cultura> {
+  bool valor = true;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Material(
-                  color: Colors.transparent,
-                  elevation: 8,
-                  borderRadius: BorderRadius.circular(50),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  child: InkWell(
-                    splashColor: Colors.green,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const Alface()),
-                      );
-                    },
-                    child: Column(
-                      children: [
-                        Ink.image(
-                          image: const NetworkImage(
-                              'https://i.pinimg.com/originals/3b/6f/00/3b6f0090835130a13f02ad2999619ac4.png'),
-                          height: 50,
-                          width: 100,
-                          fit: BoxFit.cover,
-                        ),
-                        const SizedBox(height: 1),
-                        const Text(
-                          'Alface',
-                          style: TextStyle(fontSize: 20, color: Colors.black),
-                        )
-                      ],
+    return Scaffold(
+      appBar: AppBar(backgroundColor: widget.cor, title: Text(widget.titulo)),
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.green,
+                    textStyle: const TextStyle(fontSize: 20)),
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.call_made_rounded,
+                      color: Colors.green,
+                      size: 40,
                     ),
-                  ),
-                ),
-                Material(
-                  color: Colors.transparent,
-                  elevation: 8,
-                  borderRadius: BorderRadius.circular(50),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  child: InkWell(
-                    splashColor: Colors.red,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const Tomate()),
-                      );
-                    },
-                    child: Column(
-                      children: [
-                        Ink.image(
-                          image: const NetworkImage(
-                              'https://png.pngtree.com/png-vector/20201128/ourlarge/pngtree-tomato-png-image_2405795.jpg'),
-                          height: 50,
-                          width: 100,
-                          fit: BoxFit.cover,
-                        ),
-                        const SizedBox(height: 1),
-                        const Text(
-                          'Tomate',
-                          style: TextStyle(fontSize: 20, color: Colors.black),
-                        )
-                      ],
+                    const SizedBox(
+                      width: 5,
                     ),
-                  ),
-                ),
-                Material(
-                  color: Colors.transparent,
-                  elevation: 8,
-                  borderRadius: BorderRadius.circular(50),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  child: InkWell(
-                    splashColor: Colors.yellow,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const Batata()),
-                      );
-                    },
-                    child: Column(
-                      children: [
-                        Ink.image(
-                          image: const NetworkImage(
-                              'https://www.pngall.com/wp-content/uploads/2016/04/Potato-PNG-Picture.png'),
-                          height: 50,
-                          width: 100,
-                          fit: BoxFit.cover,
-                        ),
-                        const SizedBox(height: 1),
-                        const Text(
-                          'Batata',
-                          style: TextStyle(fontSize: 20, color: Colors.black),
-                        )
-                      ],
+                    Text(
+                      'Tempo de crescimento: ${widget.dias}dias',
                     ),
-                  ),
-                ),
-              ],
-            )
-          ]),
+                  ],
+                )),
+            TextButton(
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.blue,
+                    textStyle: const TextStyle(fontSize: 20)),
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.water,
+                      color: Colors.blue,
+                      size: 40,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'Umidade necessária: ${widget.umidadeNecessaria}% ',
+                    ),
+                  ],
+                )),
+            TextButton(
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.orange,
+                    textStyle: const TextStyle(fontSize: 20)),
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.sunny,
+                      color: Colors.orange,
+                      size: 40,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'Temperatura Ideal: ${widget.temperaturaIdeal}°C',
+                    ),
+                  ],
+                )),
+            TextButton(
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.blue.shade900,
+                    textStyle: const TextStyle(fontSize: 20)),
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.water_drop,
+                      color: Colors.blue.shade900,
+                      size: 40,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'Umidade Atual: ${widget.umidadeAtual}%',
+                    ),
+                  ],
+                )),
+            buildSwitch(),
+          ],
+        ),
+      ),
     );
   }
+
+  Widget buildSwitch() => Transform.scale(
+        scale: 3,
+        child: Switch.adaptive(
+          activeColor: Colors.green,
+          activeTrackColor: Colors.green.withOpacity(0.4),
+          inactiveThumbColor: Colors.red,
+          inactiveTrackColor: Colors.red.withOpacity(0.4),
+          splashRadius: 10,
+          activeThumbImage: const AssetImage('assets/on.png'),
+          inactiveThumbImage: const AssetImage('assets/off.png'),
+          value: valor,
+          onChanged: (valor) => setState(
+            () => this.valor = valor,
+          ),
+        ),
+      );
 }
