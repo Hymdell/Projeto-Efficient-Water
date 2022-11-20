7302ecb82c78cd34a3ae33bc2ab61607115514b4
Cristopher Machado <cristopherpiussi@gmail.com>
2022-11-19 23:27:26 -0300

    mudança do nome mudança da imagem mudança dos botoes integração com web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,15 +19,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.green, title: Text(title)),
+      appBar: AppBar(backgroundColor: Colors.green, title: Text(widget.title)),
       body: const Center(
         child: Text('My Page!'),
       ),
@@ -257,7 +265,56 @@ class Cultura extends StatefulWidget {
 }
 
 class _CulturaState extends State<Cultura> {
+  final url = "https://jsonplaceholder.typicode.com/posts";
   bool valor = true;
+  var posts = [];
+  Color corEstado = Colors.red;
+  String estado = "Desligado";
+  IconData icone = Icons.block;
+
+  void fetchPosts() async {
+    try {
+      final response = await get(Uri.parse(url));
+      final data = jsonDecode(response.body) as List;
+
+      setState(() {
+        posts = data;
+      });
+    } catch (err) {}
+  }
+
+  void postData() async {
+    try {
+      final response = await post(Uri.parse(url),
+          body: {"title": "Estado", "body": "true", "userId": "1"});
+      print(response.body);
+      setState(() {
+        corEstado = Colors.green;
+        estado = "Ligado";
+        icone = Icons.done_outline_rounded;
+      });
+    } catch (err) {}
+  }
+
+  void postData2() async {
+    try {
+      final response = await post(Uri.parse(url),
+          body: {"title": "Estado", "body": "false", "userId": "1"});
+      print(response.body);
+      setState(() {
+        corEstado = Colors.red;
+        estado = "Desligado";
+        icone = Icons.block;
+      });
+    } catch (err) {}
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -281,7 +338,7 @@ class _CulturaState extends State<Cultura> {
                       width: 5,
                     ),
                     Text(
-                      'Tempo de crescimento: ${widget.dias}dias',
+                      'Tempo de crescimento: ${widget.dias} dias',
                     ),
                   ],
                 )),
@@ -345,27 +402,60 @@ class _CulturaState extends State<Cultura> {
                     ),
                   ],
                 )),
-            buildSwitch(),
+            TextButton(
+                style: TextButton.styleFrom(
+                    foregroundColor: corEstado,
+                    textStyle: const TextStyle(fontSize: 20)),
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Icon(
+                      icone,
+                      color: corEstado,
+                      size: 40,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'Estado Atual: $estado',
+                    ),
+                  ],
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text("Ligar"),
+                    IconButton(
+                      splashRadius: 40,
+                      splashColor: Colors.green,
+                      iconSize: 80,
+                      onPressed: postData,
+                      icon: Ink.image(image: const AssetImage("assets/on.png")),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text("Desligar"),
+                    IconButton(
+                      splashRadius: 40,
+                      splashColor: Colors.red,
+                      iconSize: 80,
+                      onPressed: postData2,
+                      icon:
+                          Ink.image(image: const AssetImage("assets/off.png")),
+                    ),
+                  ],
+                ),
+              ],
+            )
           ],
         ),
       ),
     );
   }
-
-  Widget buildSwitch() => Transform.scale(
-        scale: 3,
-        child: Switch.adaptive(
-          activeColor: Colors.green,
-          activeTrackColor: Colors.green.withOpacity(0.4),
-          inactiveThumbColor: Colors.red,
-          inactiveTrackColor: Colors.red.withOpacity(0.4),
-          splashRadius: 10,
-          activeThumbImage: const AssetImage('assets/on.png'),
-          inactiveThumbImage: const AssetImage('assets/off.png'),
-          value: valor,
-          onChanged: (valor) => setState(
-            () => this.valor = valor,
-          ),
-        ),
-      );
 }
