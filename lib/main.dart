583431de47a9b0ef3bf06a57ab7f902faac53cd3
Cristopher Machado <cristopherpiussi@gmail.com>
2022-11-21 21:11:56 -0300

    A

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:mysql1/mysql1.dart';
 
 void main() => runApp(const MyApp());
 
